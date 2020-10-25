24081d343b628f9e18e07eb6039a6d616b1045ef
Leanna <lrlash17@ufl.edu>
2020-10-25 17:54:55 -0400

    attempted to do the click method from the lecture

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-// It is always helpful to use comments in your code!
+var label1 = document.getElementById('main-menu-button');
+
+var label2 = document.getElementById('main-menu-button');
+
+var label3 = document.getElementById('main-menu-button');
+
+var showMePannel = document.getElementById('sub-menu-button');
+
+function openPanel(){
+	showMePannel.classList.add('main-menu');
+}
+
+label1.addEventListener('click', openPanel);
+label2.addEventListener('click', openPanel);
+label3.addEventListener('click', openPanel);
